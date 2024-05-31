28d6ed92a6aa956f1df02b68184d660781b8ea95
manit <manit@DESKTOP-M79R3F8>
2024-05-31 13:18:02 +0530

    This project is referenc for core java basic

---
+++ javabasic/src/Day5/MyClass.java
@@ -0,0 +1,30 @@
+package Day5;
+
+public class MyClass {
+      private static MyClass obj=new MyClass();
+      private int id;
+      
+      public int getId()
+      {
+    	  return id;
+      }
+      
+      public void setId(int id)
+      {
+    	  this.id=id; 
+      }
+      
+      private MyClass() {
+    	  System.out.println("MyClass object creator");
+      }
+      
+      public static MyClass getObject()//factory method
+      {
+    	  return obj;
+      }
+      
+      @Override 
+      public String toString() {
+    	  return "MyClass [id=" + id + "]";
+      }
+}
