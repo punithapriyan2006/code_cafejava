5d867c36e66fa86fa529b060ac5eeac5353f569c
manit <manit@DESKTOP-M79R3F8>
2024-06-02 10:02:30 +0530

    This project is reference for core java basic

---
+++ javabasic/src/Day9/Notepad.java
@@ -0,0 +1,30 @@
+package Day9;
+
+public class Notepad {
+
+	protected String content;
+
+    public Notepad() {
+        this.content = "";
+    }
+
+    public void write(String text) {
+        content += text; //content = content+text
+    }
+
+    public void displayContent() {
+        System.out.println("Notepad Content: " + content);
+    }
+    
+    
+    /*
+     * returntype funcName()
+     * {
+     * 		//statement
+     * }
+     * 
+     * */
+
+	}
+
+
