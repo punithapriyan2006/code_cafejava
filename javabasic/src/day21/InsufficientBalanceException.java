758a00c434ced48267605f157cd72885caaf5172
manit <manit@DESKTOP-M79R3F8>
2024-06-09 14:41:41 +0530

    This project is reference for core java basics

---
+++ javabasic/src/day21/InsufficientBalanceException.java
@@ -0,0 +1,14 @@
+package day21;
+
+public class InsufficientBalanceException extends Exception {
+
+	public InsufficientBalanceException() {
+		super("Insufficient balance in your account");
+	}
+
+	public InsufficientBalanceException(String message) {
+		super(message);
+
+	}
+
+}
